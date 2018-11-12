a473555f0dbc0fc401726d4ca9f11318d6050410
M4Z3N <42979194+TestForSomething@users.noreply.github.com>
2018-11-12 16:37:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,65 +1,243 @@
-var ServerID = "508196822242689030"; //اي دي السيرفر
-var ChannelID = "508196822242689032";// اي دي الروم
-
-/*	
- 
+    const Discord = require("discord.js");
+    const Enmap = require(`enmap`);
+    const farm = new Enmap();
 
 
-                                                                                                            
-                                                                                                            
-  ______   ______   ______   ______   ______   ______   ______   ______   ______   ______   ______   ______ 
- /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/ 
-                                                                                                            
-                                                                                                            
-
-		
-__________              __________        __   
-\______   \_______  ____\______   \ _____/  |_ 
- |     ___/\_  __ \/  _ \|    |  _//  _ \   __\
- |    |     |  | \(  <_> )    |   (  <_> )  |  
- |____|     |__|   \____/|______  /\____/|__|  
-                                \/             
-
-_________                    .___.__  __          
-\_   ___ \_______   ____   __| _/|__|/  |_  ______
-/    \  \/\_  __ \_/ __ \ / __ | |  \   __\/  ___/
-\     \____|  | \/\  ___// /_/ | |  ||  |  \___ \ 
- \______  /|__|    \___  >____ | |__||__| /____  >
-        \/             \/     \/               \/ 
+    const client1 = new Discord.Client();
+    const client_2 = new Discord.Client();
+    const client_3 = new Discord.Client();
+    const client_4 = new Discord.Client();
+    const client_5 = new Discord.Client();
+    const client_6 = new Discord.Client();
 
 
-   _____  .__                     
-  /     \ |__| ____   ___________ 
- /  \ /  \|  |/    \_/ __ \_  __ \
-/    Y    \  |   |  \  ___/|  | \/
-\____|__  /__|___|  /\___  >__|   
-        \/        \/     \/       
-
-
-*/
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('warn', console.warn);
-
-client.on('error', console.error);
-
-
-client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script','\n','By Zeyad,Royale','\n','ProBot credits miner is ready!'));// Codes Server` - iiZeyad , Roýale"1.5k 🍷#1111
-
-client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
-
-client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));// Codes server - !.Zeyad' , ♚Roýale#5555
-
-
-function timerFunc() {
-    client.on('message', msg => {
-        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
-
-
+    client1.on('ready', () => {
+    console.log(`Logged in as ${client1.user.tag}!`);
     });
-}
+    function randomIntFromInterval(min,max){
+        return Math.floor(Math.random()*(max-min+1)+min);
+    }
+    client1.on('message', msg => {
+        let ownerid = '278261303540187137';
+        if(msg.author.id === ownerid){
+            if(msg.content === "farm"){
+                if(msg.channel.id !== "508196822242689032") return;
+                let count = 0;
+                farm.set(`farm`, 'true')
+                setInterval(function(){
+                    setTimeout(function(){
+                        if(farm.get(`farm`) !== 'true') return;
+                        count++;
+                        msg.channel.send(`${randomIntFromInterval(1135,1234)}${randomIntFromInterval(1135,1334)}${randomIntFromInterval(1135,1534)} #${count}`)
+                    }, randomIntFromInterval(250, 300))
+                }, randomIntFromInterval(1119, 2119))
+            }
+            if(msg.content.startsWith(`credits`)){
+                if(msg.channel.id !== "510836021357838336") return;
+                if(msg.isMentioned(client1.user)){
+                    farm.set(`farm`, 'blah blah')
+                    let args = msg.content.split(` `)
+                    msg.channel.send(`#credits <@!${msg.author.id}> ${args[2]}`).then(()=>{
+                        const filter = m => m.author.id === msg.author.id;
+                        msg.channel.awaitMessages(filter, {max: 1, time: 29000}).then(col=>{
+                            if(col.first().content === "cancel"){
+                                return msg.reply(`Canceled!`);
+                            }
+                            msg.channel.send(`${col.first().content}`).then(()=> farm.set(`farm`, 'true'))
+                        })
+                    })
+                }
+            }
+        }
+    });
 
-var timer = setTimeout(timerFunc, 1000);
 
-client.login("NDg4NDUwNTI2MDcwMTc3ODM5.DncY6g.8E7KsA9Weq-EUREHCETz1Dv3vPs");
+    function randomIntFromInterval(min,max){
+        return Math.floor(Math.random()*(max-min+1)+min);
+    }
+    client_2.on('message', msg => {
+        let ownerid = '278261303540187137';
+        if(msg.author.id === ownerid){
+            if(msg.content === "farm"){
+                if(msg.channel.id !== "508196822242689032") return;
+                let count = 0;
+                farm.set(`farm`, 'true')
+                setInterval(function(){
+                    setTimeout(function(){
+                        if(farm.get(`farm`) !== 'true') return;
+                        count++;
+                        msg.channel.send(`${randomIntFromInterval(1135,1234)}${randomIntFromInterval(1135,1334)}${randomIntFromInterval(1135,1534)} #${count}`)
+                    }, randomIntFromInterval(250, 300))
+                }, randomIntFromInterval(1119, 2119))
+            }
+            if(msg.content.startsWith(`credits`)){
+                if(msg.channel.id !== "510836021357838336") return;
+                if(msg.isMentioned(client_2.user)){
+                    farm.set(`farm`, 'blah blah')
+                    let args = msg.content.split(` `)
+                    msg.channel.send(`#credits <@!${msg.author.id}> ${args[2]}`).then(()=>{
+                        const filter = m => m.author.id === msg.author.id;
+                        msg.channel.awaitMessages(filter, {max: 1, time: 29000}).then(col=>{
+                            if(col.first().content === "cancel"){
+                                return msg.reply(`Canceled!`);
+                            }
+                            msg.channel.send(`${col.first().content}`).then(()=> farm.set(`farm`, 'true'))
+                        })
+                    })
+                }
+            }
+        }
+    });
+
+    function randomIntFromInterval(min,max){
+        return Math.floor(Math.random()*(max-min+1)+min);
+    }
+    client_3.on('message', msg => {
+        let ownerid = '278261303540187137';
+        if(msg.author.id === ownerid){
+            if(msg.content === "farm"){
+                if(msg.channel.id !== "508196822242689032") return;
+                let count = 0;
+                farm.set(`farm`, 'true')
+                setInterval(function(){
+                    setTimeout(function(){
+                        if(farm.get(`farm`) !== 'true') return;
+                        count++;
+                        msg.channel.send(`${randomIntFromInterval(1135,1234)}${randomIntFromInterval(1135,1334)}${randomIntFromInterval(1135,1534)} #${count}`)
+                    }, randomIntFromInterval(250, 300))
+                }, randomIntFromInterval(1119, 2119))
+            }
+            if(msg.content.startsWith(`credits`)){
+                if(msg.channel.id !== "510836021357838336") return;
+                if(msg.isMentioned(client_3.user)){
+                    farm.set(`farm`, 'blah blah')
+                    let args = msg.content.split(` `)
+                    msg.channel.send(`#credits <@!${msg.author.id}> ${args[2]}`).then(()=>{
+                        const filter = m => m.author.id === msg.author.id;
+                        msg.channel.awaitMessages(filter, {max: 1, time: 29000}).then(col=>{
+                            if(col.first().content === "cancel"){
+                                return msg.reply(`Canceled!`);
+                            }
+                            msg.channel.send(`${col.first().content}`).then(()=> farm.set(`farm`, 'true'))
+                        })
+                    })
+                }
+            }
+        }
+    });
+    function randomIntFromInterval(min,max){
+        return Math.floor(Math.random()*(max-min+1)+min);
+    }
+    client_4.on('message', msg => {
+        let ownerid = '278261303540187137';
+        if(msg.author.id === ownerid){
+            if(msg.content === "farm"){
+                if(msg.channel.id !== "508196822242689032") return;
+                let count = 0;
+                farm.set(`farm`, 'true')
+                setInterval(function(){
+                    setTimeout(function(){
+                        if(farm.get(`farm`) !== 'true') return;
+                        count++;
+                        msg.channel.send(`${randomIntFromInterval(1135,1234)}${randomIntFromInterval(1135,1334)}${randomIntFromInterval(1135,1534)} #${count}`)
+                    }, randomIntFromInterval(250, 300))
+                }, randomIntFromInterval(1119, 2119))
+            }
+            if(msg.content.startsWith(`credits`)){
+                if(msg.channel.id !== "510836021357838336") return;
+                if(msg.isMentioned(client_4.user)){
+                    farm.set(`farm`, 'blah blah')
+                    let args = msg.content.split(` `)
+                    msg.channel.send(`#credits <@!${msg.author.id}> ${args[2]}`).then(()=>{
+                        const filter = m => m.author.id === msg.author.id;
+                        msg.channel.awaitMessages(filter, {max: 1, time: 29000}).then(col=>{
+                            if(col.first().content === "cancel"){
+                                return msg.reply(`Canceled!`);
+                            }
+                            msg.channel.send(`${col.first().content}`).then(()=> farm.set(`farm`, 'true'))
+                        })
+                    })
+                }
+            }
+        }
+    });
+    function randomIntFromInterval(min,max){
+        return Math.floor(Math.random()*(max-min+1)+min);
+    }
+    client_5.on('message', msg => {
+        let ownerid = '278261303540187137';
+        if(msg.author.id === ownerid){
+            if(msg.content === "farm"){
+                if(msg.channel.id !== "508196822242689032") return;
+                let count = 0;
+                farm.set(`farm`, 'true')
+                setInterval(function(){
+                    setTimeout(function(){
+                        if(farm.get(`farm`) !== 'true') return;
+                        count++;
+                        msg.channel.send(`${randomIntFromInterval(1135,1234)}${randomIntFromInterval(1135,1334)}${randomIntFromInterval(1135,1534)} #${count}`)
+                    }, randomIntFromInterval(250, 300))
+                }, randomIntFromInterval(1119, 2119))
+            }
+            if(msg.content.startsWith(`credits`)){
+                if(msg.channel.id !== "510836021357838336") return;
+                if(msg.isMentioned(client_5.user)){
+                    farm.set(`farm`, 'blah blah')
+                    let args = msg.content.split(` `)
+                    msg.channel.send(`#credits <@!${msg.author.id}> ${args[2]}`).then(()=>{
+                        const filter = m => m.author.id === msg.author.id;
+                        msg.channel.awaitMessages(filter, {max: 1, time: 29000}).then(col=>{
+                            if(col.first().content === "cancel"){
+                                return msg.reply(`Canceled!`);
+                            }
+                            msg.channel.send(`${col.first().content}`).then(()=> farm.set(`farm`, 'true'))
+                        })
+                    })
+                }
+            }
+        }
+    });
+
+    function randomIntFromInterval(min,max){
+        return Math.floor(Math.random()*(max-min+1)+min);
+    }
+    client_6.on('message', msg => {
+        let ownerid = '278261303540187137';
+        if(msg.author.id === ownerid){
+            if(msg.content === "farm"){
+                if(msg.channel.id !== "508196822242689032") return;
+                let count = 0;
+                farm.set(`farm`, 'true')
+                setInterval(function(){
+                    setTimeout(function(){
+                        if(farm.get(`farm`) !== 'true') return;
+                        count++;
+                        msg.channel.send(`${randomIntFromInterval(1135,1234)}${randomIntFromInterval(1135,1334)}${randomIntFromInterval(1135,1534)} #${count}`)
+                    }, randomIntFromInterval(250, 300))
+                }, randomIntFromInterval(1119, 2119))
+            }
+            if(msg.content.startsWith(`credits`)){
+                if(msg.channel.id !== "510836021357838336") return;
+                if(msg.isMentioned(client_6.user)){
+                    farm.set(`farm`, 'blah blah')
+                    let args = msg.content.split(` `)
+                    msg.channel.send(`#credits <@!${msg.author.id}> ${args[2]}`).then(()=>{
+                        const filter = m => m.author.id === msg.author.id;
+                        msg.channel.awaitMessages(filter, {max: 1, time: 29000}).then(col=>{
+                            if(col.first().content === "cancel"){
+                                return msg.reply(`Canceled!`);
+                            }
+                            msg.channel.send(`${col.first().content}`).then(()=> farm.set(`farm`, 'true'))
+                        })
+                    })
+                }
+            }
+        }
+    });
+
+    client1.login("NDg4NDQ2NDk5MTEwMzg3NzEz.DncU-w.tXd241v3TEyUsg2AISVAKVR4")
+    client_2.login("NDg4NDQ2OTIzNDc3NjE0NTky.DncVwQ.hXqIrM_uGT-jfKt0cHyUo")
+    client_3.login("NDAzNTgxOTI2MjUxOTU0MTc5.DUJYsg.Q20PLeDd2vo-HRsNpAG_8nU")
+    client_4.login("NDg4NDQ3ODE2NTIwMzAyNTky.DncWQw.Ct-qSo81rg_21FmSnI")
+    client_5.login("NDgwMTY0MjgzNzIwNjYzMDQx.DncXvQ.e-hZRQgC2hnCu9DBtMTw")
+    client_6.login("NDg4NDQ5OTMwODg1ODU3MzEw.DncYSg.etk1SsUO")
